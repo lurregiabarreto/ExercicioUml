6ae36e7a782b83d79db684707f29248a62c98c55
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-09-23 13:13:12 -0300

    Criando a classe guerreiros

--- src/br/com/zup/Heroi.java
+++ src/br/com/zup/Heroi.java
@@ -1,2 +1,39 @@
-package br.com.zup;public class Heroi {
+package br.com.zup;
+
+public class Heroi {
+
+
+    private String nome;
+    private double vida;
+
+    public Heroi(String nome, double vid) {
+        this.nome = nome;
+        this.vida = vida;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+    public void trocarNome( String nome){
+
+    }
+    public void receberDano(double dano){
+
+    }
+    public void exibirVida(){
+        double vidas;
+    }
 }
