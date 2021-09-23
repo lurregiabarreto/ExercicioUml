10cfb543ddd9bd3a4731db9f163bd8235f642947
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-09-23 13:23:04 -0300

    Criando a classe Rasteiros

---
+++ src/br/com/zup/Rasteiros.java
@@ -0,0 +1,22 @@
+package br.com.zup;
+
+public class Rasteiros extends Inimigos {
+
+
+    private double danosEspinhos;
+
+    public Rasteiros(String nome, double vida) {
+        super(nome, vida);
+    }
+
+    public double getDanosEspinhos() {
+        return danosEspinhos;
+    }
+
+    public void setDanosEspinhos(double danosEspinhos) {
+        this.danosEspinhos = danosEspinhos;
+    }
+
+    private void aplicarDanos() {
+    }
+}
